b13ffd06a556889ccaee24951ca7ab566461c2ac
CptNelson <toni.lappalainen@gmail.com>
2022-01-07 13:35:40 +0200

    check for duplicate emails

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 const form = document.getElementById('form');
-const formEvent = form.addEventListener('submit', (event) => {
+const inputs = [...form.querySelectorAll('input')];
+const submitButton = document.getElementById('submit-button');
+const responseText = document.getElementById('response');
+
+const checkForEmptyInputs = () => {
+	const isIncomplete = inputs.some((input) => !input.value);
+	submitButton.disabled = isIncomplete;
+};
+
+form.addEventListener('input', checkForEmptyInputs);
+
+form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	console.log(form[2].value);
-	if (!validateEmail(form[2].value)) return;
+	if (!validateEmailBeforeSending(form[0].value)) return;
 
 	const formDataJSON = JSON.stringify(Object.fromEntries(new FormData(form)));
 
-	sendMail(formDataJSON);
+	sendFormData(formDataJSON);
 });
 
-const validateEmail = (emailAddress) => {
+const validateEmailBeforeSending = (emailAddress) => {
 	//RFC 2822 standard email validation
 	const emailFormat =
 		/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 	if (emailAddress.match(emailFormat)) {
 		return true;
 	} else {
-		alert('You have entered an invalid email address!');
+		responseText.style.color = 'red';
+		responseText.textContent = 'Invalid email address!';
 		return false;
 	}
 };
 
-const sendMail = async (formData) => {
-	console.log(formData);
-
-	const rawResponse = await fetch('https://httpbin.org/post', {
+const sendFormData = async (formData) => {
+	const response = await fetch('https://myrrys.fi/emailform/api/v1/emails', {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json',
@@ -32,15 +41,17 @@ const sendMail = async (formData) => {
 		},
 		body: formData,
 	});
-	const content = await rawResponse.json();
+	informSubmitResultsToUser(response);
+};
 
-	console.log(content);
-	/*
-	fetch('http://localhost:3666/send', {
-		method: 'post',
-		body: mail,
-	}).then((response) => {
-		return response.json();
-	});
-	*/
+const informSubmitResultsToUser = async (response) => {
+	if (response.ok) {
+		responseText.style.color = 'green';
+		responseText.textContent = 'Email submitted!';
+	} else {
+		const content = await response.json();
+		console.log(content);
+		responseText.style.color = 'red';
+		responseText.textContent = `An error occured: ${content.message.message}`;
+	}
 };
